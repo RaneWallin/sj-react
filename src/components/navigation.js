04e1a754820889bdd1b73187d84e0d7b3ccddea0
Rane Wallin <rane@ranewallin.com>
2019-02-20 06:15:21 -0700

    stling nav and adding open/close functionality

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import '../styles/navigation.scss';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/navigation.scss";
 
 class Navigation extends Component {
+  state = {
+    navOpen: false
+  };
 
-    render() {
-        return (
-            <div className="constrained">
-                <div className="hamburger-menu">
-                    <div className="nav-logo">S&J</div>
-                    <div className="hamburger"></div>
-                </div>
-                <div className="nav-links">
-                    <NavLink to='/' className='links' activeClassName='active-links'>Home</NavLink>
-                    <NavLink to='/services' className='links' activeClassName='activeLinks'>Services</NavLink>
-                    <NavLink to='/contact' className='links' activeClassName='active-links'>Contact</NavLink>
-                </div>
-            </div>
-        );
+  getNavImage = () => {
+    let imgUrl;
+
+    if (this.state.navOpen) imgUrl = require("../img/nav-hamburger-close.png");
+    else imgUrl = require("../img/nav-hamburger.png");
+
+    return <img src={imgUrl} />;
+  };
+
+  toggleNavigation = () => {
+    let navState = !this.state.navOpen;
+    this.setState({ navOpen: navState });
+  };
+
+  toggleLinks = () => {
+    let navMenu = <div />;
+    if (this.state.navOpen) {
+      navMenu = (
+        <div className="nav-links constrained">
+          <NavLink to="/" className="links" activeClassName="active-links">
+            Home
+          </NavLink>
+          <NavLink
+            to="/services"
+            className="links"
+            activeClassName="activeLinks"
+          >
+            Services
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className="links"
+            activeClassName="active-links"
+          >
+            Contact
+          </NavLink>
+        </div>
+      );
     }
 
+    return navMenu;
+  };
+
+  render() {
+    return (
+      <div className="constrained">
+        <div className="hamburger-menu">
+          <div className="nav-logo">S&J</div>
+          <div onClick={this.toggleNavigation} className="hamburger">
+            {this.getNavImage()}
+          </div>
+        </div>
+        {this.toggleLinks()}
+      </div>
+    );
+  }
 }
 
 export default Navigation;
