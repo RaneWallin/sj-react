65912e70e94507318b5bde6695741bb5d95c94aa
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-23 08:00:11 -0700

    adding services intro text

--- src/pages/services.js
+++ src/pages/services.js
@@ -3,6 +3,9 @@ import Navigation from "../components/navigation";
 import PageHolder from "../components/page-holder";
 import Header from "../components/header";
 
+// styles
+import '../styles/services.scss';
+
 //images
 import headerImg from "../img/services/services-jumbotron.png";
 import Footer from "../components/footer";
@@ -14,6 +17,13 @@ class Services extends Component {
         <PageHolder>
           <Navigation />
           <Header headerImage={headerImg} />
+            <div className="service-intro">
+                <p>
+                    Services include: completely synergize resource taxing relationships via premier niche markets.
+                    Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate
+                    resource-leveling customer service for state of the art customer service.
+                </p>
+            </div>
           <Footer />
         </PageHolder>
       </div>
