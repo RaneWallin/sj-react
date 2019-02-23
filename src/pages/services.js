a041114d6b738bb505a6d8cfcfdfef5c93910ac1
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-23 09:04:16 -0700

    starting tabs component

--- src/pages/services.js
+++ src/pages/services.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import Navigation from "../components/navigation";
 import PageHolder from "../components/page-holder";
 import Header from "../components/header";
+import Tab from "../components/tab";
 
 // styles
-import '../styles/services.scss';
+import "../styles/services.scss";
 
 //images
 import headerImg from "../img/services/services-jumbotron.png";
@@ -17,13 +18,21 @@ class Services extends Component {
         <PageHolder>
           <Navigation />
           <Header headerImage={headerImg} />
-            <div className="service-intro">
-                <p>
-                    Services include: completely synergize resource taxing relationships via premier niche markets.
-                    Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate
-                    resource-leveling customer service for state of the art customer service.
-                </p>
-            </div>
+          <div className="service-intro">
+            <p>
+              Services include: completely synergize resource taxing
+              relationships via premier niche markets. Professionally cultivate
+              one-to-one customer service with robust ideas. Dynamically
+              innovate resource-leveling customer service for state of the art
+              customer service.
+            </p>
+          </div>
+          <div className={"tabs"}>
+            <Tab tabStles={{ flexBasis: "22%" }} tabName={"Pre-Construction"} />
+            <Tab tabStles={{ flexBasis: "22%" }} tabName={"Construction"} />
+            <Tab tabStles={{ flexBasis: "22%" }} tabName={"Sustainability"} />
+            <Tab tabStles={{ flexBasis: "22%" }} tabName={"Design Build"} />
+          </div>
           <Footer />
         </PageHolder>
       </div>
